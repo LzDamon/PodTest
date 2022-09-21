c2187dd06d30d1591577d7247bfa0e22c223c381
DamonLee <lizhen02@keep.com>
2022-09-21 18:18:50 +0800

    newfile

---
+++ PodTest/Classes/Person.h
@@ -0,0 +1,18 @@
+//
+//  Person.h
+//  PodTest
+//
+//  Created by DamonLee on 2022/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+- (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PodTest/Classes/Person.m
@@ -0,0 +1,17 @@
+//
+//  Person.m
+//  PodTest
+//
+//  Created by DamonLee on 2022/9/21.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+- (void)test {
+    NSLog(@"%s", __func__);
+}
+
+@end
+
